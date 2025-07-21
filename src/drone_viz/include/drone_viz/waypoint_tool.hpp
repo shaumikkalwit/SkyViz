784ea8c020d2184a5fe4621e86ef5511b7c10b4f
Shaumik Kalwit <shaumik.kalwit@gmail.com>
2025-07-21 09:17:20 -0500

    wip: baseline main, auton, and teleop buttons/panels working

--- src/drone_viz/include/drone_viz/waypoint_tool.hpp
+++ src/drone_viz/include/drone_viz/waypoint_tool.hpp
@@ -1,36 +1,23 @@
-#pragma once
+#ifndef DRONE_VIZ__WAYPOINT_TOOL_HPP_
+#define DRONE_VIZ__WAYPOINT_TOOL_HPP_
 
 #include <rviz_common/tool.hpp>
-#include <geometry_msgs/msg/point_stamped.hpp>
-#include <rclcpp/rclcpp.hpp>
-#include <rviz_rendering/render_window.hpp>
 
 namespace drone_viz
 {
 
 class WaypointTool : public rviz_common::Tool
 {
+  Q_OBJECT
 public:
   WaypointTool();
-  ~WaypointTool() override;
+  ~WaypointTool() override = default;
 
   void onInitialize() override;
   void activate() override;
   void deactivate() override;
-
-  int processMouseEvent(rviz_common::ViewportMouseEvent & event) override;
-
-  geometry_msgs::msg::PointStamped getLastClickedPoint() const;
-  bool hasNewPoint() const;
-
-private:
-  rclcpp::Node::SharedPtr ros_node_;
-  rclcpp::Publisher<visualization_msgs::msg::Marker>::SharedPtr marker_pub_;
-
-  geometry_msgs::msg::PointStamped clicked_point_;
-  bool point_ready_ = false;
-
-  void publishMarker(const geometry_msgs::msg::PointStamped& point);
 };
 
-} // namespace drone_viz
+}  // namespace drone_viz
+
+#endif  // DRONE_VIZ__WAYPOINT_TOOL_HPP_
