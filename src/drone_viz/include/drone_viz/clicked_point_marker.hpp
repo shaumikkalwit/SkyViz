37084517f28f9f81130f54827004b3d92dd29cc7
Shaumik Kalwit <shaumik.kalwit@gmail.com>
2025-07-25 10:15:20 -0500

    prog: rviz2 config properly working lol

--- src/drone_viz/include/drone_viz/clicked_point_marker.hpp
+++ src/drone_viz/include/drone_viz/clicked_point_marker.hpp
@@ -14,12 +14,20 @@ public:
 
   void undo_last_marker();
 
+  bool has_last_point() const;
+
+  geometry_msgs::msg::PointStamped get_last_point() const;
+
 private:
   void point_callback(const geometry_msgs::msg::PointStamped::SharedPtr msg);
-    rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr undo_service_;
-    void handle_undo_request(
-      const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
-      std::shared_ptr<std_srvs::srv::Trigger::Response> response);
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr undo_service_;
+  void handle_undo_request(
+    const std::shared_ptr<std_srvs::srv::Trigger::Request> request,
+    std::shared_ptr<std_srvs::srv::Trigger::Response> response);
+
+  bool has_point_;
+  geometry_msgs::msg::PointStamped last_clicked_point_;
+  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr get_point_service_;
 
 
   rclcpp::Subscription<geometry_msgs::msg::PointStamped>::SharedPtr point_sub_;
