6084f0b084a706887d6fd4beff2ee3c3d9947b14
Shaumik Kalwit <shaumik.kalwit@gmail.com>
2025-08-12 16:27:45 -0500

    up to date

--- src/drone_viz/include/drone_viz/flight_client_node.hpp
+++ src/drone_viz/include/drone_viz/flight_client_node.hpp
@@ -10,72 +10,72 @@
 
 
 class FlightClientNode {
-    private:
-        std::atomic<bool> thread_running_state;
-        std::shared_ptr<rclcpp::Node> node_;
-        rclcpp::Client<drone_viz_interfaces::srv::FlightService>::SharedPtr client_;
-        
-        std::shared_ptr<drone_viz_interfaces::srv::FlightService::Request> request_;
-    public:
-        FlightClientNode() {
-            node_ =  rclcpp::Node::make_shared("flight_service_client");
-            client_ = node_ -> create_client<drone_viz_interfaces::srv::FlightService>("flight_service");
-            request_ = std::make_shared<drone_viz_interfaces::srv::FlightService::Request>();
-            thread_running_state = false;
-        }
-        void threadedRequest(const drone_viz_interfaces::msg::Command & command) {
-          if (thread_running_state) {
-              RCLCPP_ERROR(node_ -> get_logger(), "Command already pending, request denied.");
-              return;
-            }
-            std::thread t(&FlightClientNode::sendRequest, this, command);
-            t.detach();
-        }
-        void sendRequest(drone_viz_interfaces::msg::Command command) {
-            //check if a request is already pending
-            
-            thread_running_state = true;
-            request_ -> command_input = command;
-           
-            while (!client_ -> wait_for_service(std::chrono::seconds(1))) {
-                if (!rclcpp::ok()) {
-                    RCLCPP_ERROR(node_ -> get_logger(), "Interrupted while waiting for the service. Exiting.");
-                } else {
-                  RCLCPP_INFO(node_ -> get_logger(), "Service appears to be unavailable... quitting.");
-                  thread_running_state = false;
-                  return;
-                  }
+  private:
+      std::atomic<bool> thread_running_state;
+      std::shared_ptr<rclcpp::Node> node_;
+      rclcpp::Client<drone_viz_interfaces::srv::FlightService>::SharedPtr client_;
+      
+      std::shared_ptr<drone_viz_interfaces::srv::FlightService::Request> request_;
+  public:
+      FlightClientNode() {
+          node_ =  rclcpp::Node::make_shared("flight_service_client");
+          client_ = node_ -> create_client<drone_viz_interfaces::srv::FlightService>("flight_service");
+          request_ = std::make_shared<drone_viz_interfaces::srv::FlightService::Request>();
+          thread_running_state = false;
+      }
+      void threadedRequest(const drone_viz_interfaces::msg::Command & command) {
+        if (thread_running_state) {
+            RCLCPP_ERROR(node_ -> get_logger(), "Command already pending, request denied.");
+            return;
+          }
+          std::thread t(&FlightClientNode::sendRequest, this, command);
+          t.detach();
+      }
+      void sendRequest(drone_viz_interfaces::msg::Command command) {
+          //check if a request is already pending
+          
+          thread_running_state = true;
+          request_ -> command_input = command;
+          
+          while (!client_ -> wait_for_service(std::chrono::seconds(1))) {
+              if (!rclcpp::ok()) {
+                  RCLCPP_ERROR(node_ -> get_logger(), "Interrupted while waiting for the service. Exiting.");
+              } else {
+                RCLCPP_INFO(node_ -> get_logger(), "Service appears to be unavailable... quitting.");
+                thread_running_state = false;
+                return;
                 }
-               
-            
-            auto result = client_ -> async_send_request(request_);
-
-            //wait for result
-            rclcpp::executors::SingleThreadedExecutor executor;
-            executor.add_node(node_);
-
-            while (rclcpp::ok()) {
-              executor.spin_some(std::chrono::milliseconds(100));
-              if (result.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
-                break;
               }
-            }
+              
+          
+          auto result = client_ -> async_send_request(request_);
 
+          //wait for result
+          rclcpp::executors::SingleThreadedExecutor executor;
+          executor.add_node(node_);
+
+          while (rclcpp::ok()) {
+            executor.spin_some(std::chrono::milliseconds(100));
             if (result.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
-              auto response = result.get();
-              if (response->success) {
-                RCLCPP_INFO(node_ -> get_logger(), "SUCCESS!");
-                RCLCPP_INFO(node_ -> get_logger(), "Message: %s", response->message.c_str());
-              }
-              else {
-                RCLCPP_INFO(node_ -> get_logger(), "FAIL!");
-                RCLCPP_INFO(node_ -> get_logger(), "Failed, error: %s", response->message.c_str());
-              }
+              break;
             }
-            
-            thread_running_state = false;
-        }
-        ~FlightClientNode() {}
+          }
+
+          if (result.wait_for(std::chrono::seconds(0)) == std::future_status::ready) {
+            auto response = result.get();
+            if (response->success) {
+              RCLCPP_INFO(node_ -> get_logger(), "SUCCESS!");
+              RCLCPP_INFO(node_ -> get_logger(), "Message: %s", response->message.c_str());
+            }
+            else {
+              RCLCPP_INFO(node_ -> get_logger(), "FAIL!");
+              RCLCPP_INFO(node_ -> get_logger(), "Failed, error: %s", response->message.c_str());
+            }
+          }
+          
+          thread_running_state = false;
+      }
+      ~FlightClientNode() {}
 };
 
 #endif // FLIGHT_CLIENT_NODE_HPP
